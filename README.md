2a38d1eb8c856f19e3ccb274e89ef26d726291ce
Pratik Jadhav <prateek7120@gmail.com>
2024-06-21 16:22:05 +0530

    bug_fixed

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ Before excuting, install following in you system
 
 - [Git](https://git-scm.com/)
 - [Visual Studio](https://visualstudio.microsoft.com/downloads/) 
-
+Note - Same button will be used as a toggle switch to resume and pauses the stop watch without setting it to  0.
 1st Page ( **Home Page**)
 ![image](./images/Window%20-%20Form1.png)
 
--- obj/Debug/net8.0-windows/slidely_task02_frontend.AssemblyInfo.vb
+++ obj/Debug/net8.0-windows/slidely_task02_frontend.AssemblyInfo.vb
@@ -16,7 +16,7 @@ Imports System.Reflection
 <Assembly: System.Reflection.AssemblyCompanyAttribute("slidely_task02_frontend"),  _
  Assembly: System.Reflection.AssemblyConfigurationAttribute("Debug"),  _
  Assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0"),  _
- Assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83ac6c0e92154864928e0834195f926b2ba97763"),  _
+ Assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1f69831a6f453f3a9656e86c6844f6acb91fdec"),  _
  Assembly: System.Reflection.AssemblyProductAttribute("slidely_task02_frontend"),  _
  Assembly: System.Reflection.AssemblyTitleAttribute("slidely_task02_frontend"),  _
  Assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0"),  _
